f0fa743ab1861621c0a29d6e0214916e6600efbe
Guillermo <memo33101@gmail.com>
2018-09-11 08:04:41 -0600

    added method and variable to model class

--- src/model/MarshmallowMonster.java
+++ src/model/MarshmallowMonster.java
@@ -2,5 +2,15 @@ package model;
 
 public class MarshmallowMonster
 {
-
+	public MarshmallowMonster()
+	{
+		
+	}
+	
+	public String toString()
+	{
+		String description = "";
+		
+		return description;
+	}
 }
